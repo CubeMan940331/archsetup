e8c928f15dc705d18931da46ddc6a32e235da676
CubeMan <CubeMan940331@gmail.com>
2026-01-20 01:13:10 +0800

    chmod action.sh in config-all.sh

--- config-scripts/config-all.sh
+++ config-scripts/config-all.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SCRIPT_FILE="$(basename "${BASH_SOURCE[0]}")"
-cd $SCRIPT_PATH
+cd "${SCRIPT_PATH}" || exit 1
 
 echo "install pkgs"
-pacman -S --noconfirm --needed $(cat ./*/pkg.txt | grep -v '^#')
+pacman -S --noconfirm --needed "$(cat ./*/pkg.txt | grep -v '^#')"
 echo "" > /var/archsetup_install.log
 while read -r item; do
     item_name=$(basename "$item" | sed -e 's/^[0-9]*-//g')
     if [ ! -e "$item/action.sh" ]; then
         continue
     fi
-    echo "$item_name" | tee -a /var/archsetup_install.log
+    echo "${item_name}" | tee -a /var/archsetup_install.log
+    chmod +x "${item}/action.sh"
     if ! (
-        "$item/action.sh" 2>&1 | tee -a /var/archsetup_install.log
+        "${item}/action.sh" 2>&1 | tee -a /var/archsetup_install.log
     ); then
         echo "script in $item_name failed"
         echo "check /var/archsetup_install.log"

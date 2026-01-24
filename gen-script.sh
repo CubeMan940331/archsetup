f6414cd517419e4a609a63c1cac61867b5650e60
CubeMan <CubeMan940331@gmail.com>
2026-01-24 17:35:55 +0800

    make gen-script.sh works on macOS

--- gen-script.sh
+++ gen-script.sh
@@ -1,11 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env sh
 for item in config-scripts/*/action.sh; do
     chmod +x "$item"
 done
 chmod +x config-scripts/config-all.sh
-exa -D1 config-scripts > config-scripts/list.txt
+find config-scripts -depth 1 -type d | \
+    sed -e 's:^config-scripts/::' -e 's:/$::' | \
+    sort > config-scripts/list.txt
 
 cp install-script-template.sh install.sh
-echo "archive='$(tar -czO config-scripts | base64 -w0)'" >> install.sh
+echo "archive='$(tar -cf - config-scripts/ | gzip -9 | base64 -w0)'" >> install.sh
 echo 'main "$@"' >> install.sh
 chmod +x install.sh

5045e346d55bbf5144a014f63061102a69f254ac
cubeman <CubeMan940331@gmail.com>
2026-02-21 23:21:33 +0800

    avoid installing unused pkgs

--- config-scripts/config-all.sh
+++ config-scripts/config-all.sh
@@ -3,7 +3,14 @@ SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${SCRIPT_PATH}" || exit 1
 
 echo "install pkgs"
-cat ./*/pkg.txt | grep -v '^#'  | pacman -S - --noconfirm --needed
+pkg_files=""
+while read -r item; do
+    if [ -e "${item}/pkg.txt" ]; then
+        pkg_files="${pkg_files} ${item}/pkg.txt"
+    fi
+done < "list.txt"
+cat $pkg_files | grep -v '^#'  | pacman -S - --noconfirm --needed
+
 echo "" > /var/archsetup_install.log
 while read -r item; do
     item_name=$(basename "$item" | sed -e 's/^[0-9]*-//g')

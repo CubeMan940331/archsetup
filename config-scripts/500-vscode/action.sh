a8b2ed46f1ce1812f0fa7ddbbe9de7f92af7b08f
CubeMan <CubeMan940331@gmail.com>
2026-07-24 22:12:29 +0800

    add exec permission

--- config-scripts/500-vscode/action.sh
+++ config-scripts/500-vscode/action.sh
@@ -6,5 +6,6 @@ NAME=$(basename "${SCRIPT_PATH}" | sed -e 's/^[0-9]*-//g')
 echo "start executing ${NAME} script"
 
 # script =======================
-cp files/update-vscode /usr/bin &&
-/usr/bin/update-vscode > /dev/null
+cp files/update-vscode /usr/bin && 
+chmod +x /usr/bin/update-vscode &&
+/usr/bin/update-vscode

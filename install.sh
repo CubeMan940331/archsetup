8a10a3c0b0e06e41f5088e8c2d738d730ac24fd3
CubeMan <CubeMan940331@gmail.com>
2026-07-25 11:41:55 +0800

    regenerate install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-host_name="CubicSilicon"
+host_name="archPC"
 user_name="user"
 
 user_passwd="user"
@@ -15,11 +15,27 @@ mirror_list=(
     'https://archlinux.cs.nycu.edu.tw/$repo/os/$arch'
 )
 
+set -eo pipefail
 SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-cd "${SCRIPT_PATH}" || exit 1
+cd "${SCRIPT_PATH}"
+function print_info(){
+    tput setaf 4
+    for item in "$@"; do echo "${item}"; done
+    tput sgr0
+}
+function print_fail(){
+    tput setaf 1
+    for item in "$@"; do echo "${item}"; done
+    tput sgr0
+}
+function print_success(){
+    tput setaf 2
+    for item in "$@"; do echo "${item}"; done
+    tput sgr0
+}
 
 function setting_mirror(){
-    echo "setting mirror"
+    print_info "setting mirror"
     # setting mirror
     if [[ ! -e "reflector-result.txt" ]]; then
         reflector --country "${mirror_locations}" --latest 8 -p https --save reflector-result.txt
@@ -69,10 +85,8 @@ function edit_basic_conf(){
 }
 
 function extract(){
-    echo "extract scripts"
-    (echo "${archive}" | base64 -d | tar -xz --skip-old-files) || \
-    (echo "failed to extract scripts"; exit 1)
-    
+    print_info "extract scripts"
+    echo "${archive}" | base64 -d | tar -xz --skip-old-files
     edit_basic_conf
 }
 
@@ -83,19 +97,17 @@ function install_base(){
         return
     fi
     setting_mirror
-    echo "install base packages"
+    print_info "install base packages"
     
-    timedatectl &&
-    pacstrap -K /mnt base linux &&
-    genfstab -U /mnt > /mnt/etc/fstab &&
-    echo "install at $(date)" > /mnt/root/finished.txt ||
-    exit 1
+    timedatectl
+    pacstrap -K /mnt base linux
+    genfstab -U /mnt > /mnt/etc/fstab
+    echo "install at $(date)" > /mnt/root/finished.txt
 }
 
 function install_all(){
-    echo "copy config-scripts/ to /mnt/root/"
-    cp -r "${SCRIPT_PATH}/config-scripts" /mnt/root ||
-    exit $?
+    print_info "copy config-scripts/ to /mnt/root/"
+    cp -r "${SCRIPT_PATH}/config-scripts" /mnt/root
     
     arch-chroot /mnt "/root/config-scripts/config-all.sh"
     ret=$?
@@ -115,7 +127,7 @@ function main(){
         base    | 2) state="2" ;;
         install | 3) state="3" ;;
         *)
-            echo "invalid operation"
+            print_fail "invalid operation"
             echo "valid operations: extract, base, install"
             exit 1
         ;;
@@ -125,10 +137,8 @@ function main(){
     if [[ "${state}" -ge "2" ]]; then install_base; fi
     if [[ "${state}" -ge "3" ]]; then
         install_all
-        tput setaf 2
-        echo "Installation Finish!"
-        tput sgr0
+        print_success "Installation Finish!"
     fi
 }
-archive='H4sIAAAAAAACA+1ce3PbNhL33/gUiOKz495AJCWTumur9Bwnk+aaJpk4ubk5n6uhSPARkQSPACU5ib97FyBF6xHHbUaWfDZ2xhYBLJ4L/LBYPDyWBXFIuFfEueDGzk2QCdSzbflr9Wxz/ndGO5bd6ZpOz7Q61o5pdR2ru4PtnQ1QyYVbYLzjlUOautmVfNeF/5+Styj/2ukmSZtHG5K/ZZv2kvwdy3F2sKnlf+P08IExjDNj6PIInRy/ffHm3eDN0buf+63dR56P4b8fF5mbUvj89OTo5OfByev3b4+fnZpnF62DFt7bw/nEP2ghxfxpLoWLFv78GdNpLLCFEPUihltxBo2dJDgfhbyF4P8giBPK+60WmkTwhQvq+pgUOBY0/QH7DGGgOMCnmKgSSP8LAyK2xVS08NkPWEQ0U1yS5lLc/dQ4LvByRBUhiJHPMop/xK0k5qIK8FyBd5uY+DMOC5pjMsb7vz3cx+DOXQ96ASYnmGBCMqYGTJHKb0p96s+q2sKPsTF2C8MtvIhTUeaDuvbthIVfrS44BrLJ+7uPQCy0bn3pDU2KOfVlY+xz47dTk/z97DtiGOH+wWVLPajaSvIbridilsFQXm0rKLiIs5LO2kL+VEWvWksVQQoRC0oxcb9WG5VelDIf/3V6KabLzGeFe4AfNfl/gQ13Hu9ZfzjDg6UKVYWvgAzHGd5taoEDF1rbby2xehH1Rl/JZY6/6sVX9pq7g/+WaRLOI2PL8/9hz3T0/L9F+ddIuT75O4eHV8sfZL40/3fgR8//GyCW0wwEjiREdoZuhnY03Se6Yvw3k/Im8N+2l/HfsTsdPf7vkP7/6ujXZ4v67DLvlYptrdeBlAQkR70S1NYQVHqZ5AWuOm4LoYf1J+5/mYDBY/k5loo98nL1yw3o6z7oep6bYIMKz5jBoPEBPtq+ARWUSUOhh6Cvc1qMY48ifs5Bt/RE0gRAMpJ1JeDWw+oV479qna3pf45pmVr/27r8L0fHjep/ltNdkn+v59ga/zdBp1LGZ6iCKx/3sShKikD8ghbgkqEI8KvSB8ADeonk5ODpjWgmY1Sg56PUnRZUFOfgZUMKmS9iQPo+tiKZQu2wI61h3uLxD6ORwCQde+u0APx5/LetnqXxf6vyX6MF4Fr8763Y/21L2/83o/9jJW5p1x65ocJ1TlESZ+UU8dJnUvs9fvMep7FXMI/5FLmpT0r1FWeCJvU3sEk7+MQt6tikcUJQRsWEFSNU/0I7Ql6Fnglu8fhfowXguv0/ZwX/e6ap1/96/b/W9T9nZeFBztXSRnXytuz9Ku470E7/wzLArgwTHmCj5IXBI4Av4yN4x1nADCizVGIH0gNKrOwFanEkfVE08cAxwoRIfZhFnkxWBYM6zao9PrktBalUvgO5f3T6D2jHZutvrgke7s82yFr7xsI3/m+zL/XjXCFoO6TZXNhjbIg0nw/b21OB6XglZCkVtcOFKjeR7loGL49ePe/vfvJp4JaJGFQMF2qncy4B2aay7hHjQsoazXYVpcdsV7GO0vAAf8GYgGmI84nfRJF+g8pP9ZHqEySkuGUs1/dxyWEukf+kg6TP8SSiNJHxpWedZZOm8vtimksRVq05r6rp69d6+kLpCEYHJnlVGTld0oLXVqNLExN4E06F7LZ8mRPdWvxfowXoG+w/nZ4+/3Mb5H8Jkzeo/3eW9/86Zkfr/5vS/5upr5nc+nNT19xc1X90gBaxv7+A8w2+K2+JpMq7gVTlXQP8HLD30Rwi9/Wa4FaN/4XJ66bGv9ntLI1/y9T7/5uhv1S60tHLl/1H8O97+DvA0qkH4n0c/x0Y/788fbb181+2ZWv9b4vy3+j5r67jrOz/ArvG/43of3lBxzSDpXfBUugCWDAMK2UujSzynDbK45xO4oKSD643Qv8TDklLZfLwY5cEQZrTEGVMMBKwTPC5T+J9GCGUJy5PXcR9P5X63/P3L7Cb5xyNWMYZKJg+S/IozlAAWQRsinhOYaVO/JjnrvAiWGaHE5qNYzpB48RDIWSrFv6BF4upjRZ+CPBPoOAz57Cg9CNtAlVXF4wlem67bvxv9PyX0+ksj/9uV+t/2v67VvuvvBwxux1RWeEAkpRpo+0bLXw2f0FiybA3xwg4hb58lAwip5X1czXWHz1GJkHyVoz/LZ//sh2rp/W/rcv/skvfqP5n2sv7/z27Z2r83wSdvpMyPkNPQd9K3PMTgCZa9CfueeJmPjouiwJ0w36tSGl16e7b/ywSFuVwrQaAb8B/09bnf7cq/3UaAP60/Rf0f33+dzMkhY0YJ7D6H8JamwbxkDGRhvpw1v3G/3UaAK47/2U6K/v/jqPX/3r9v9b1v+zWZPYCRXVOihBINaSiP/2bM3AOCaBfEwLfBOpOPcGK874hUbEJk46EuT4tSOz3ZcJygX+zGcyCCpqysbIWSJvCoqGi3p03ZIS2v3AKqYb4theEV/Gq8qejChEwYViVSTFUXBBXzwr3BP/XaQD6hvNf4NT6//blP4caN6r/26vnv/T5743Q87fvnwyevjg5evLy2eD1yeDN29dPnr3tB27Ctb3n/uH/7E2jteZx7fjvNfhv2TAXmJbd7ejznxuh5sAfmkE/ggUZASU74ai+DY7qXQEEwiFjru576ZFzN+f/mey3fP+3Y+v7v9uV/wbv/zrmYSN/ufEr7T+mtv9uxv6DfTqmCZbHoqrLv0R5oLA+ZiXXAeqcQs3iMx7w6pNOA1fA+iDkKBMBJ91QxhB0KrA8pcUKNI7VoS/5Yqab+Vheo8O5G1IEPsQfqp9cXTuGWDJU2XEimiT4/b9laSLisTRPqDRGIpGWU8jUnbtRjKuJClykKpNXFgmahFSgOKdF0EXCy/0yzatDG2leUM7lSxZVvI+xCqgrCD1BlLwOigTL0TCFbDPPL1HgchFQ4UWqPjH3kHwzcmbumY0fVNAgURYdNPSQl7qiiKfIYxPuniMYZ1ARxBN58GNIM0hrErGYo1s0/i9neGOr+H9o6v2/Lct/fTsA177/Zl3K/1D6W72OpfF/M/h/b+z/jSm8zH1X0LqbVze9oQlkVVDziPHMd4l5bw99OQTdvfG/PgvwN9h/bVPbf2+F/Bd6+Q3p/4edlft/+v3/LeD/PNIKhn02yeQmJP7XybEEP6nwxmPaUjo2JoSVIi+F3MNUD0qAj1wLSAWbDKFJyet/vsT7kRA5/94wZPdpj2NeugkXpR+zNmjj8oUNY5bNT8MyTvw+V+/L7THer54RmjqH+0iKiHycBtXLFTItUvF914agdvgRk+O5NzskTquHPObf8ahqQZpUVb0XpgBDW7Y0adKkSZMmTZo0adKkSZMmTZo03TH6Hca2/RwAeAAA'
+archive='H4sIAAAAAAACA+1ce3PbuBH33/gUiJL6cR2IFCVK7d0pPcfJ5NLmkkycdDp1fRqKBB8xSbAEKMlx/N27ACmKkuO4yciSz8FOYhFPAljsD4vFEi5L/Sgg3M2jTHBj5zbIBBrYtvztDGyz+TunnY5tdc3+wOxYnR2z0+13ujvY3tkAFVw4OcY7bjGmiZNem++m9D8oucv8r4JOHLd5uCH+W2Z3UPJ/YA7gH/C/b5nWDjY1/2+dHj4wxlFqjB0eIk4FJpThLMqo70QxOj56++LNu9Gbw3e/DluP9l0Pw18vylMnofB48eTw+NfR8ev3b4+enZinl62DFt7dxdnUO2ghlfmiUcNlC3/6hOksEriD/CJ1RcRSnOVRKkZR6rP9gwuEgURWCAxNcXzcUxE+y3EkaIKjFKr8pfUT9himbsjkC2TCpYpKaaN4kJvoEqFF7bgVpcDpOMbZWcBbCP6O/CimfNhqoWkITzinjodJ+S5Zoaou8vEJDEr9KgMKtsVMtPDpT1iENFW5JDVqfHRRBy7xasGyTxGSLcY/41YccVEmuI7Aj+qS+BMOcpphMsF7vz/cwxDOHBemICbHmGBCUqakNU/kM6Ue9RAqh6WFH2Nj4uSGk7shDGWRjaret2MWfLG7EBhJ9g4f7cOcoBWnZTSwD9jiycHY48bvJyb56+kPxDCCvYPFSD0ox0rmNxzFYcCRq2MFDRdRWtD5WKjxa/BKyqTAeZGmURpUI6iadVkOX9nNL+TCj784AqoNYcI8/OfZgrWLBs879ADv123+TDZsPd7twLAISjFxbnrhwcogNCZ6p46seqbgWE74Zq+wFErqtVZqkHN9ubgbUvfsC81Z1FCJ47VTckfT/aaV9b9jmoTzcL164Nfrf72B2df63xb5Xy1W6+N/v9e7nv/A82X+9y340frfBohlNAWGI7m0WGMn1Yiv8R/kv9ZxNoH/Nmz25/s/q9uX8m93tfzf5/3fq8Pfni1vMVazXrvXeIgr7Xj4eYIMLsvOsdxEITdTv9yASe2B6us6MTaocI053hkf4KHtGQiKQVPGsDHiNJ9ELkX8nIPq7Yq4ToA6rsb+sZHzGvkvB21r+l/f7Jha/9s6/xdCc6v6X6ffXeH/YNC3Nf5vgk4kj09RiWUeHmKRFxQB+wXNIaQgD8Ct1AcgAmaJzMkh0j2jqSxRIqKHEmeWU5GfQ5QNNaSeiADZh7gTyhqqgB1qDfMOyz9II4FFOXLXaQH4evy3O4OOxv+t8n+NFoAb8X9gr67/dqev8X8j+j9W7JZHC2dOoHCdUxRHaTFDvPCY1IuP3rzHSeTmzGUeRU7ikUI9RamgcfUM2eRRxNTJq9KkDkJSSsWU5Weo+oVxhHfleiW4w/K/RgvADfjfse1Ovf+3e12p/5mWqeVf7/+/af/PWZG7UGW5h1GzuS2neUsi0TtQQ/8tz4rjFBPuY6PgucFDwCnjI0TL40cDGiO11ZGMgKYoe4HaBclYFE5dCJxhQqTiy0JXVquSQW9eOqq+KGNH8jjt5JfT8phanbU1+vZwrz7F3jOWnvF/6mO6nxuNoO2Apo20x9gQSdZM291VicnkSspKLerAD5VhIsPV+fHLw1fPh48uPJgFRSxGZYZLdarcqECOqex7yLiQTETzM3kZUZ/DlkXqPJA/Z0zAesP51KuLyLhRGaeYXz4Ch1RuWcrxPFxwWDTkHxkgyXM8DSmNZXkZOT8gntep4j5b50qBqzadV+U69Vu1TqHkDGY9JlnZGbku0py3PUMO9cLEBNEEZEhEacBXc6I/Gv6v0QL0DfYfa6D9v+4C/xfoeYv6v7V6/meZltb/N6X/1ytiveYNGytaYwkb7h+g5SVhuAT/NeyraAmwKrpGWhVd4X4D74eoAdRDvSe4U/K/tKbdlvybXWtF/jumPv/fDP2pVKEOX74c7sOfH+H/AZZBLYjfo/xbIP//ePps6/5fdsfW+t8W+b9R/69uv3/l/Beya/zfiP6X5XRCU9iR5yyBKYAFw7CB5tKlWrrKI2kNmkY5JR8c9wz9V/RJUihLiBc5xPeTjAYoZYIRn6WCNx6J++EMoSx2eOIg7nmJ1P+ev3+BnSzj6IylnIGC6bE4C6MU+fAKn80Qzyhs4IkX8cwRbgi772BK00lEp2gSuyiA1yp7gO9GYmajpR8C+afQ8HlwnFP6kdaJaqoLxmK9tt0k/5v1/5rj/8Ac9E1Lyn9X+3/da/vvjUZc+TXJ/HOS0pQGAKIMEW3PaOHT5hclK9a5RkZAFfR5fzAonJQmzKulpFXv/3EHk5B2T+V/y/5fdr8z0Prf1vm/EJJb1f9Me/X8f2AP9PnfRujkneTxKXoK+lbsnB8D2NF8OHXOYyf10FGR56AbDitFSqtL99/+1yFBXozXagD4BvwHnVDj/zb5v04DwFfbf0H/1/6/myHJbMQ4gd3/GPba1I/GjIkk0M5Z3zf+r9MAcJP/F+D9wv/LVvLfH+j9/33e/9+m/5ecv2R+20fpJ0UIMDmgYjj7S3/U7xGAuToFngn0ibqC5edDQ8JfnSYDMXM8mpPIGyqsvNXa66ScJmwirQyrvkfVAbwhc7e9hT2jgvC26wefzaianZyV4o4Jw6opKrXMAgU15Gv8n+P/Og1A3+D/BUGt/2+f/w1UuVX9377q/6Xv/9oIPX/7/sno6Yvjwycvn41eH4/evH395Nnboe/EXNt7vj/8n9/8tNZ33Cj/gxL/O2YXoL8n7T9dS8v/Rqh2+ENz6EewISOFiGKOqq/BUXUqgGz4nXD1vZeWnPu5/s95v+Xvfy1bf/+7Xf5v8Pvfvtmr+S8PfqX9x9T2383Yf7BHJzTG0i2q/PiXqAgUVG5Wch+gPB+qLB7jPi8f6cx3BOwPAo5S4XPSDWQJQWcCSy8tlqNJpJy+5KWlTuphdbln5gQUQQzxxuonU58dQymZKnPzkMYxfv8v2ZqQuCzJYiqNkUgkxQxe6jS+KMblQgUhUrbJLfIYTQMqUJTR3O8i4WZekWSlG0iS5ZRzeZNFWe5jpBKqDsJMEAWvkkLBMjRO4LWp6xXId7jwqXBD1Z+Iu0jerDk3BM3lB+XUj5WtB41d5CaOyKMZctmUO+cI5Aw6gngsXUnGNIW6piGLOLpD8r9Y4Y2t4n/P1Od/W+b/+k4AbuJ/z7YW/n+W9P8bWJb2/90M/t8/+39tEy8yzxG0ms/ll97QV1Rf/DyPWc6Iron+LvT/hvyvzwL8DfZf29T23zvB/yUZuCX9v2dd+f4PdgAa/zeN/81b9QXDHpum8ngS//P4SAKoVHijCW0pHRsTwgp5/7w83VT3TECM3AtIBZuMYUjJ67+/xHuhEBn/0TDk9GlPIl44MReFF7E2aOPy4g1j/pq/jYso9oZc3S+3y/iwvEZo1u/tIcki8nHmlxdayLpIme+HNiS1g4+YHDWu8pAO3Op+j+b1HmUvSF2r6vfSymBoy5YmTZo0adKkSZMmTZo0adKk6R7S/wAwWhbAAHgAAA=='
 main "$@"
